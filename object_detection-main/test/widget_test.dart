eafda6b83f2b00972c680d4408600d6e9833377a
Ronaldo Carreño Miranda <ronaldoc@uninorte.edu.co>
2021-11-10 23:10:17 -0500

    feat: widget_tests
    Añadidos tests de widgets de prueba

--- object_detection-main/test/widget_test.dart
+++ object_detection-main/test/widget_test.dart
@@ -13,18 +13,15 @@ import 'package:object_detection/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MaterialApp(home: MyApp()));
 
     // Verify that our counter starts at 0.
-    expect(find.text('0'), findsOneWidget);
-    expect(find.text('1'), findsNothing);
+    expect(find.text('Real'), findsOneWidget);
+    expect(find.text('Imagen'), findsOneWidget);
 
-    // Tap the '+' icon and trigger a frame.
-    await tester.tap(find.byIcon(Icons.add));
+    await tester.tap(find.text('Real'));
     await tester.pump();
-
-    // Verify that our counter has incremented.
-    expect(find.text('0'), findsNothing);
-    expect(find.text('1'), findsOneWidget);
+    await tester.tap(find.text('Imagen'));
+    await tester.pump();
   });
 }
